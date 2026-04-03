992fb27ed1bab015a67df854b56c50c22df4d088
Thaly <thalyann.olivo@gmail.com>
2026-04-03 16:54:20 -0400

    Edited App.jsx to a more descriptive header name.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import './App.css';
+import PatientSummary from './pages/PatientSummary';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>App</h1>
+        <h1>Capsule: Patient Summary</h1>
       </header>
       <main>
+        <PatientSummary patientId={1} />
       </main>
     </div>
   );
